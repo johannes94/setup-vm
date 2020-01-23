e3327fd1895d90856dd8988f03ae0a276e48156b
Johannes Malsam <johannes.malsam.b@gmail.com>
2020-01-23 23:26:49 +0100

    Add install for vs code and golang

--- setup.sh
+++ setup.sh
@@ -1,6 +1,33 @@
+#install vim 
 sudo apt install -y vim
 
 #set vim as default editor
 echo "export EDITOR=$(which vim)" >> ~/.profile
 #disable terminal alert sounds
 echo "bind 'set bell-style none'" >> ~/.bashrc
+
+#install docker community edition
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update -y
+sudo apt install -y docker-ce
+
+# allow docker commands without prefixing with sudo
+sudo usermod -aG docker $USER
+
+# install visual studio code
+sudo snap install --classic code
+
+# install golang
+sudo snap install --classic go
